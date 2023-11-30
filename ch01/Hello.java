6fb91a57f9ac28d8d9e2f65046288be3c3a01689
Suyoung-Kim <150898767+suweeety@users.noreply.github.com>
2023-11-30 22:13:30 +0900

    Update Hello.java
    일부 삭제

--- ch01/Hello.java
+++ ch01/Hello.java
@@ -13,7 +13,7 @@ public class Hello { // 클래스는 동작메서드를 모아 놓는 객체
 		// main 쓰고 컨트롤 스페이스 누르면 자동완성
 		String a = "";
 		a = "a";
-		System.out.println("안녕하세요. 자바지옥에 오신걸 환영합니다.");
+		
 		System.out.println("안녕하세요. 자바지옥에 오신걸 환영합니다.");
 		// sysout -> 컨트롤 스페이스를 누르면 자동완성
 
